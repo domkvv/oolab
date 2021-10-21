615fbb48dd10b8c2b4a391090cfb9ddc64e080d3
domkvv <niczkaa.bo@gmail.com>
2021-10-21 13:59:09 +0200

    to task 13.

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -1,7 +1,27 @@
 package agh.ics.oop;
 
 public class World {
+    public static void main(String[] args) {
+        System.out.println("Start");
+        run(args);
+        System.out.println("Stop");
+    }
 
+    public static void run(String[] args) {
+        System.out.println("Zwierzak idzie do przodu");
+        boolean flag = true;
+        for (String i : args) {
+            if (flag) {
+                System.out.print(i);
+                flag = false;
+            } else {
+                System.out.print(", ");
+                System.out.print(i);
+            }
+        }
+        System.out.println();
+
+    }
 
 }
 
